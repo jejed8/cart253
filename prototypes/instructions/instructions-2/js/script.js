18ce17a9637cb3bf444d3cf09ea42cdd2b18adff
jejed8 <47800238+jejed8@users.noreply.github.com>
2026-09-22 18:41:41 -0400

    prototype > instructions-2 : created heart shape and loop
    created a loop that draws a heart and scale it by 0.05 each iteration

--- prototypes/instructions/instructions-2/js/script.js
+++ prototypes/instructions/instructions-2/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Title of Project
  * Author Name
- * 
+ *
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
@@ -9,21 +9,52 @@
 "use strict";
 
 /**
- * Create a 500x500 pixel grey canvas
-*/
+ * Create a 600x600 pixel grey canvas
+ */
 function setup() {
+  //create a canvas of 500x500px
+  createCanvas(600, 600);
 
-    //create a canvas of 500x500px
-    createCanvas(500, 500);
-
-    //color of the canvas
-    background('grey');
+  //color of the canvas
+  background("grey");
 }
 
+//sets a base value for the scale to iterate upon
+let scaleValue = 0.05;
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * draws the heart loop
+ */
 function draw() {
+  push();
 
+  //centers and scales the heart shape
+  translate(300, 300);
+  scale(scaleValue);
+  translate(-300, -300);
+
+  // changes the colour of the heart
+  fill(0, 0, 200);
+
+  //removes the outline of the shape
+  noStroke();
+
+  /*
+  * Draws the heart shape
+  * credits : https://www.youtube.com/watch?v=EqoykdvW7eE
+  */
+  triangle(300, 300, 100, 300, 300, 100);
+  triangle(300, 300, 100, 300, 300, 500);
+  triangle(300, 300, 500, 300, 300, 100);
+  triangle(300, 300, 500, 300, 300, 500);
+  circle(200, 200, Math.sqrt(200 * 200 + 200 * 200));
+  circle(400, 200, Math.sqrt(200 * 200 + 200 * 200));
+
+  // closes the shape  
+  pop();
+
+  // creates a loop that grows the scale of each heart until it reaches 3
+  if (scaleValue < 1) {
+    scaleValue += 0.05;
+  }
 }
