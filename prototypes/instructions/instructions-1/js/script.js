c4aa46e6072d321c55cfed0a53828aca915096a7
jejed8 <47800238+jejed8@users.noreply.github.com>
2026-09-22 16:55:03 -0400

    Drawing of the reference points for cherry stems
    - Added 3 reference points to help draw the stems (left cherry, right cherry and middle point)

--- prototypes/instructions/instructions-1/js/script.js
+++ prototypes/instructions/instructions-1/js/script.js
@@ -38,6 +38,7 @@ function draw() {
     //closes the shape
     pop();
 
+
     // right cherry
     push();
     //colour of the shape
@@ -48,4 +49,36 @@ function draw() {
     ellipse(460, 500, 100, 100);
     //closes the shape
     pop();
+    
+
+    //reference points for drawing the stems
+    push();
+    //colour of the points
+    stroke("blue");
+    //size of the points
+    strokeWeight(15);
+
+    //point for left cherry
+    point(340, 450);
+
+    //point for right cherry
+    point(460, 450);
+
+    //middle point
+    point(400, 300);
+
+    pop();
+
+
+
+    // left cherry stem
+    push();
+    //colour of the stem
+    stroke("#22613a");
+    //thickness of the stem
+    strokeWeight(3);
+    //
+
+
+
 }
