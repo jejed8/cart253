99bde31e1dd85b69eb4b09471cebca17c1a5e266
jejed8 <47800238+jejed8@users.noreply.github.com>
2026-09-22 20:10:07 -0400

    draw a coin

--- prototypes/instructions/instructions-3/js/script.js
+++ prototypes/instructions/instructions-3/js/script.js
@@ -1,7 +1,7 @@
 /**
  * Title of Project
  * Author Name
- * 
+ *
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
@@ -11,20 +11,35 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
+
+
 function setup() {
+  // Create a 500x500px canvas
+  createCanvas(500, 500);
 
-    // Create a 500x500px canvas 
-    createCanvas(500, 500);
-
-    //changes the colour of the canvas
-    background('grey');
-
+  //changes the colour of the canvas
+  background(0, 0, 0);
 }
-
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
-function draw() {
 
+
+function draw() {
+  //Coin
+  push();
+
+  //draw and position the coin
+  circle(250, 250, 200);
+
+  //set the colour of the coin
+  fill("#FAB805");
+
+  //set the colour of the outline
+  stroke("#E09D0D");
+
+  //set the thickness of the outline
+  strokeWeight(15);
+  //pop();
 }
